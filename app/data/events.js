291d2f9e6cebef810f8a012ea37ba4aed069896d
Rudi Firdaus <rudifirdaus233@gmail.com>
2026-06-24 12:36:12 +0700

    feat: add event details for Open Recruitment MBC Lab 2026/2027

--- app/data/events.js
+++ app/data/events.js
@@ -2,9 +2,9 @@ export const events = [
   {
     id: 1,
     title: 'Open Recruitment MBC Lab 2026/2027',
-    date: '???',
-    location: '???',
-    description: '???',
+    date: '24/06/2026',
+    location: 'Telkom University, Bandung',
+    description: 'Join us for our open recruitment event! We are looking for talented individuals to join our team and help us achieve our goals. Don\'t miss this opportunity to be part of something great!',
     tag: 'Recruitment',
   },
 ]
